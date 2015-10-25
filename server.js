f91666dbb6b1c388a43ee6d4baf7688b785779b8
Ken Hoff <thekenhoff@gmail.com>
2015-10-25 15:52:09 -0700

    sending response

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 app = express();
 
+app.get("/", function (req, res) {
+	res.send("Hello!")
+})
+
 app.listen(3000 || process.env.PORT, function () {
 	console.log("Listening...");
 })
