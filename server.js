a841be58475fb1dd975956dbef59e37d184bbfce
Ken Hoff <thekenhoff@gmail.com>
2015-10-25 16:24:06 -0700

    first whack at web sockets

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app = express();
-
-app.get("/", function (req, res) {
-	res.send("Hello!")
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/index.html")
 })
 
-app.listen(process.env.PORT || 3000, function () {
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+	socket.on("message", function (data) {
+		console.log("message:", data);
+		socket.send("i got your message")
+	})
+});
+
+http.listen(process.env.PORT || 3000, function() {
 	console.log("Listening...");
 })
