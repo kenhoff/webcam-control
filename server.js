7fca4587cc216fd5813c49bc0c9d990c6d9314c8
Ken Hoff <thekenhoff@gmail.com>
2015-10-25 15:53:26 -0700

    reversed ports

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("/", function (req, res) {
 	res.send("Hello!")
 })
 
-app.listen(3000 || process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log("Listening...");
 })
