8835f61f0daa20e71a4ecb7033c4a3ca7866ea53
Ken Hoff <thekenhoff@gmail.com>
2015-11-07 17:45:03 -0800

    pushing directions to clients

--- socket-client.js
+++ socket-client.js
@@ -39,7 +39,7 @@ client.connect(42124, '127.0.0.1', function() {
 
 client.on('data', function(data) {
 	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
+	// client.destroy(); // kill client after server's response
 });
 
 client.on('close', function() {
--- socket-server.js
+++ socket-server.js
@@ -1,21 +1,51 @@
-/*
-In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp
-server, but for some reason omit a client connecting to it.  I added an
-example at the bottom.
-Save the following server in example.js:
-*/
+
+commands = [
+	"left",
+	"right",
+	"up",
+	"down"
+]
+
 
 var net = require('net');
 
 var server = net.createServer();
 
+connections = []
+
 server.on("connection", function(socket) {
+	connections.push(socket)
+	// console.log(connections);
 	socket.setEncoding("utf8")
 	socket.on("data", function (data) {
 		console.log(data)
 		socket.write('Received:\t' + data + "\n");
 	});
+	socket.on("close", function () {
+		console.log("socket done");
+		// pop this socket from connections
+		connections.splice(connections.indexOf(socket), 1)
+	})
+	socket.on("error", function (err) {
+		console.log("error:", err);
+	})
 })
 
+server.on("error", function (err) {
+	console.log(err);
+})
+
+
+writeSomething = function () {
+	console.log("connections:", connections.length);
+	for (var i = 0; i < connections.length; i++) {
+		// console.log(connections[i])
+		connections[i].write(commands[Math.floor(Math.random() * commands.length)])
+	}
+}
+
+
+
+setInterval(writeSomething, 1000)
 
 server.listen(42124);
