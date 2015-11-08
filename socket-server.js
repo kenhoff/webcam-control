57a5b289c90b82eb46dcebd31e9fe485475836d4
Ken Hoff <thekenhoff@gmail.com>
2015-11-07 16:52:01 -0800

    tcp sockets

---
+++ socket-server.js
@@ -0,0 +1,17 @@
+/*
+In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp
+server, but for some reason omit a client connecting to it.  I added an
+example at the bottom.
+Save the following server in example.js:
+*/
+
+var net = require('net');
+
+var server = net.createServer();
+
+server.on("connection", function(socket) {
+	console.log("someone connected!");
+	socket.pipe(socket)
+})
+
+server.listen(42124, '127.0.0.1');
